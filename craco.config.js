401bec2aa6c580acbb63a35437706f7f7bf4035e
Sanath Shetty <sanath.shetty@xebia.com>
2021-06-04 20:05:10 +0530

    Added Antd configuration in craco

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,26 @@
 // craco.config.js
+
+const CracoLessPlugin = require("craco-less");
+
 module.exports = {
+  // * Configuration for Tailwind
   style: {
     postcss: {
       plugins: [require("tailwindcss"), require("autoprefixer")],
     },
   },
+  // * Configuration for Antd
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { "@primary-color": "#1DA57A" },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
 };
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,13 @@
+import { Button } from "antd";
+import "../styles/App.less";
 function App() {
-  return <div className="App">My App</div>;
+  return (
+    <div className="App bg-red-400	">
+      My App
+      <br></br>
+      <Button type="primary">Button</Button>
+    </div>
+  );
 }
 
 export default App;
